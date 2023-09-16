eb681d4e393185d5d54562be524f3948abeb209c
pranittare <tarepranit505@gmail.com>
2023-09-16 23:58:30 +0530

    added icons

--- components/ads/guarantee.tsx
+++ components/ads/guarantee.tsx
@@ -1,7 +1,10 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import { detailsProps } from '@/constants/types'
-
+import certificate from '../../assets/certificate.png'
+import truck from '../../assets/truck.png'
+import priceTag from '../../assets/price-tag.png'
+import Image from 'next/image'
 
 const dummy: detailsProps[] = [
     { description: '24k Guaranteed Quality Certified', icon: 'some icon' },
@@ -10,19 +13,19 @@ const dummy: detailsProps[] = [
     { description: 'Order Tracking & Support', icon: 'some icon' }
 ]
 const dummy2: detailsProps[] = [
-    { description: '24k Guaranteed Quality Certified', icon: 'some icon' },
-    { description: '100% Secure with Delivery Insurance', icon: 'some icon' },
-    { description: 'Best Prices in the market', icon: 'some icon' },
+    { description: '24k Guaranteed Quality Certified', icon: certificate },
+    { description: '100% Secure with Delivery Insurance', icon: truck },
+    { description: 'Best Prices in the market', icon: priceTag },
 ]
 
 const Guarantee = ({smallSize}:{smallSize:boolean}) => {
     if (smallSize) {
         return <Flex justifyContent={'space-between'} flexWrap={'wrap'}>
         {dummy2.map(item => {
-            return <Box key={item.description} maxWidth={'150px'}>
-                <Text>{item.icon}</Text>
-                <Text>{item.description}</Text>
-            </Box>
+            return <Flex justifyContent={'center'} direction={'column'} alignItems={'center'} key={item.description} maxWidth={'150px'} >
+                <Box height={'60px'} width={'60px'}><Image src={item.icon} alt={item.description} /></Box>
+                <Text textAlign={'center'}>{item.description}</Text>
+            </Flex>
         })}
     </Flex>
     }
